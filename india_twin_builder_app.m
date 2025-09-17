e3b208bccab4f41b4c6c73c1e7d1952a8c74e252
ShreyashSri <shreyash.sri09@gmail.com>
2025-09-17 17:07:53 +0530

    Import fixed

--- india_twin_builder_app.m
+++ india_twin_builder_app.m
@@ -247,7 +247,7 @@ function onEditPersona(fig)
     
     try
         % Add Python path and RELOAD module
-        pythonScriptPath = 'C:\ILoveCoding\kyaMATLAB\kyaMATLAB';
+        pythonScriptPath = 'C:\codes\SIH\kyaMATLAB';
         if count(py.sys.path, pythonScriptPath) == 0
             insert(py.sys.path, int64(0), pythonScriptPath);
         end
@@ -287,7 +287,7 @@ function onExport(fig)
     
     try
         % Add Python path and RELOAD module
-        pythonScriptPath = 'C:\ILoveCoding\kyaMATLAB\kyaMATLAB';
+        pythonScriptPath = 'C:\codes\SIH\kyaMATLAB';
         if count(py.sys.path, pythonScriptPath) == 0
             insert(py.sys.path, int64(0), pythonScriptPath);
         end
@@ -321,7 +321,7 @@ function onRun(fig)
     
     try
         % Add Python path and RELOAD module
-        pythonScriptPath = 'C:\ILoveCoding\kyaMATLAB\kyaMATLAB';
+        pythonScriptPath = 'C:\codes\SIH\kyaMATLAB';
         if count(py.sys.path, pythonScriptPath) == 0
             insert(py.sys.path, int64(0), pythonScriptPath);
         end
